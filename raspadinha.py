d325e58ea9428bf55d966f246d14fa9d04fc95d0
Bernardo Giancristofaro Carvalho <giancristofarobernardo@gmail.com>
2026-06-02 23:36:53 -0300

    correção imagem

--- raspadinha.py
+++ raspadinha.py
@@ -60,13 +60,31 @@ let surpresaLiberada = false;
 foto.src = "data:image/jpeg;base64,{foto}";
 cover.src = "data:image/jpeg;base64,{cover}";
 
-foto.onload = () => {{
-    ctx.drawImage(foto, 0, 0, canvas.width, canvas.height);
+foto.onload = () => {
 
-    cover.onload = () => {{
-        ctx.drawImage(cover, 0, 0, canvas.width, canvas.height);
-    }}
-}}
+    console.log("Foto carregada");
+
+    cover.onload = () => {
+
+        console.log("Cover carregado");
+
+        ctx.drawImage(
+            foto,
+            0,
+            0,
+            canvas.width,
+            canvas.height
+        );
+
+        ctx.drawImage(
+            cover,
+            0,
+            0,
+            canvas.width,
+            canvas.height
+        );
+    };
+};
 
 
 function getPos(e) {{
